a5f9f23a2297fa8a69b0e9c25f6c60e99ab1fa3f
Sachin Katheriya <sachincse2@gmail.com>
2019-01-28 16:24:33 +0530

    Created

---
+++ Account.java
@@ -0,0 +1,18 @@
+public class Account{
+	private long accountNumber;
+	private double accountBalanace;
+
+	public long setAccountNumber(long accountNumber){
+		this.accountNumber=accountNumber;
+	}
+	public long getAccountNumber(){
+		return accountNumber;
+	}
+	
+	public double setAccountBalance(long accountBalance){
+		this.accountBalance=accountBalance;
+	}
+	public double getAccountBalance(){
+		return accountBalance;
+	}
+}
